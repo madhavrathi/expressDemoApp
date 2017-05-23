7eca16c3edcb51476e89bcda43906107cc7d4f10
Madhav Rathi <mdhvrthi@gmail.com>
2017-05-23 22:12:55 +0530

    Post requests now feed new friends

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 //adding requirements
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-//telling express to use files inside public directory
+//telling express to use files inside public directory and other dependencies
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+var friends = ["maddy" , "tom" , "Jack"
+                , "chuck" , "bobby"];
 // routes
 app.get("/",function(req,res){
   res.send("This is HOME PAGE !");
 });
 
 app.get("/friends",function(req,res){
-  var friends = ["maddy" , "tom" , "Jack"
-                  , "chuck" , "bobby"];
   res.render("friends.ejs",{friends:friends});
-});
-
-app.post("/addfriend",function(req,res){
-  res.send("you have reached the post route");
 });
 
 app.get("/home/:parameter",function(req,res){
@@ -30,7 +28,16 @@ app.get("/home/:parameter",function(req,res){
   //will be used in ejs file and the thing
   //on the right is thing containing value
   //of parameter
+});
 
+app.post("/addfriend",function(req,res){
+  friends.push(req.body.newfriend);
+  res.send("you have reached the post route");
+  //req.body contains details entered in post request
+  //initially we get undefined because express does
+  //automatically converts it into js object
+  //to do that install (npm install body-parser --save)
+  console.log(req.body);
 });
 
 
